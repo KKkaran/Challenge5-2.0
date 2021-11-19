7dd06079f469f8871940d7060cb250a6634110e0
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-19 01:49:20 -0500

    locally storing values and also making sure changes made only when changes are made

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var tasks = []//list of all the  updated tasks objects
+var timetable = []//list derived from local storage at the time of page refresh
+
 //try to get the current day using moment.js and update #currentDay
 var currentDay = moment().format("dddd, MMMM DD YYYY")
 $("#currentDay").html(currentDay)
@@ -17,26 +20,172 @@ function createTimeBlock(hour){
     }else if(hour < 12){
         am = "am"
     }
-    console.log(`${newhour}${am}`)
     //create the hour
     var containerSlot = $("<div>").
-                        addClass("d-flex justify-content-between contain")
+                        addClass("d-flex justify-content-between contain").
+                        attr("id",hour)
 
     var hourSlot = $("<p>").
-                    addClass("hour").
+                    addClass("timestamp hour").
                     html(`${newhour}${am}`)
     
     var textSlot = $("<p>").
-                    addClass("entry").
-                    html(newhour,am)
+                    addClass("evententry")
 
     var buttonSlot = $("<button>").
-                    addClass("btn saveBtn").
+                    addClass("btn saveBtn lock").
                     html("Lock")
 
     containerSlot.append(hourSlot,textSlot,buttonSlot)
     $(".container").append(containerSlot)
 
 }
+//modify on task click
+$(".container").on("click",".evententry",function(){
+    
+    var text = $(this).text().trim()
+
+    var textarea = $("<textarea>").
+                    addClass("textarea").
+                    val(text)
+    $(this).closest("div").children("button").css("background","red")
+    $(this).closest("div").children("button").html("Save")
+
+    $(this).replaceWith(textarea)
+    textarea.trigger("focus")
+})
+$(".container").on("blur",".textarea",function(){
+    
+    var text = $(this).val().trim()
+    var hour = $(this).closest("div").attr("id")
+    var hourid = `#${hour}`
+    var task = $("<p>").
+                addClass("evententry").
+                html(text)
+    $(this).replaceWith(task)
+    //no changes made after triggering textarea change the color of button back to normal
+    // if(){
+    var ggg;
+    tasks.forEach(el =>{
+        if(el.id === hour){
+            
+            ggg = el.text
+            boolean = true
+        }
+    })
+    if(text === ggg ){
+        //console.log("no changes made")
+        $(hourid).children("button").css("background","#06aed5")
+        $(hourid).children("button").html("Lock") 
+    }else if(!text){
+        $(hourid).children("button").css("background","#06aed5")
+        $(hourid).children("button").html("Lock") 
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(".container").on("click",".lock", function(){
+
+    $(this).closest("div").children("button").css("background","#06aed5")
+    $(this).closest("div").children("button").html("Lock")
+
+    var text = $(this).closest("div").children(".evententry").text().trim()
+    var hour = $(this).closest("div").attr("id")
+    
+    var ggg;
+    var index = -1;
+    var myindex
+    var boolean = false
+    tasks.forEach(el =>{
+        index++;
+        if(el.id === hour){
+            myindex = index
+            ggg = el.text
+            boolean = true
+        }
+    })
+    if(timetable.length === 0){
+        if(!text){
+            //console.log("nothin entred")
+            return
+        }
+    }else{
+        if(text === ggg ){
+            //console.log("no changes made")
+            return
+        }else if(!boolean){
+            if(!text){
+                //console.log("empty")
+                return
+            }
+        }
+    }
+
+    if(boolean){
+        tasks[index][ggg]
+    
+        //console.log(`index is: ${index}`)
+        var obj = {}
+        obj.id = hour
+        obj.text = text
+    
+        tasks.splice(myindex,1,obj)
+    }else{
+        var obj = {}
+        obj.id = hour
+        obj.text = text
+        tasks.push(obj)
+    }
+    
+    saveTasks() 
+})
+
+
+//locally store the updated tasks
+//$(this).closest("div").children("button").css("background","red")
+//$(this).closest("div").children("button").html("Save")
+function saveTasks(){
+    localStorage.setItem("schedule",JSON.stringify(tasks))
+}
+function loadTasks(){
+
+    var task = localStorage.getItem("schedule")
+    if(!task){
+        //console.log("nothing")
+        return
+    }
+
+    timetable = JSON.parse(task)
+    tasks = timetable
+    timetable.forEach(element => {
+        var hour = element.id
+        var text = element.text
+        var hourid = `#${hour}`
+        $(hourid).children(".evententry").html(text)
+
+    });
+    console.log(timetable)
+}
 
 getHours()
+loadTasks();
